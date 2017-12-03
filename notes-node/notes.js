624e09de713d0b3b8ade054abecc2aa8063ee862
Christopher.Bischoff <christopher.bischoff@phishme.com>
2017-12-03 13:51:27 -0700

    update/notes-node-3-10
    - created simple sum function

---
+++ notes-node/notes.js
@@ -0,0 +1,12 @@
+console.log('Starting notes.js');
+
+module.exports.addNote = () =>  {
+    console.log('addNote');
+    return 'New Note';
+};
+
+//accept input; return sum
+module.exports.addNumbers = (a,b) => {
+    console.log('addNumbers');
+    return a+b;
+};
