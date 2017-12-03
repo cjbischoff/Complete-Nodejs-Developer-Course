99caf4e63ec5c6d7737036d9f8c6ec1922619cf7
Christopher.Bischoff <christopher.bischoff@phishme.com>
2017-12-03 16:12:20 -0700

    update/notes-node Class 3-12
    - learned about nodemon
    - cleanup up code from previous lessons

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,30 +1,6 @@
 console.log('Starting apps.js');
 
-// load module functionality to be used
 const fs = require('fs');
-const os = require('os');
+const _ = require('lodash');
+
 const notes = require('./notes');
-
-// Add Note Function
-var res = notes.addNote();
-console.log(res);
-
-// Add numbers
-var sum = notes.addNumbers(5,5);
-console.log(sum);
-
-// Add numbers
-console.log('Result:',notes.addNumbers(9,-2));
-
-// var user = os.userInfo();
-// console.log(user);
-
-// Warnings for this code
-// fs.appendFile('greetings.txt', 'Hello-World');
-
-// No warnings
-// fs.appendFileSync('greetings.txt','Hello' + user.username + '!');
-
-// Better way
-// fs.appendFileSync('greetings.txt',` Hello ${user.username}! You are ${notes.age}.`);
-
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -4,9 +4,3 @@ module.exports.addNote = () =>  {
     console.log('addNote');
     return 'New Note';
 };
-
-//accept input; return sum
-module.exports.addNumbers = (a,b) => {
-    console.log('addNumbers');
-    return a+b;
-};
