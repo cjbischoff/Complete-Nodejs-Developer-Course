c1e2739bd7b97f2ded75bf34f1179b4708d29ef2
Christopher.Bischoff <christopher.bischoff@phishme.com>
2017-12-04 10:34:00 -0700

    update/notes-node 3-14
    - using yargs
    - created initial function framework

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,23 +2,25 @@ console.log('Starting apps.js');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes');
 
-var command = process.argv[2];
+const argv = yargs.argv;
+var command = argv._[0];
 
+//Verbose debugging
 console.log('Command:', command);
-
-console.log(process.argv);
+console.log('Yargs', argv);
 
 if (command === 'add') {
-    console.log('Adding new note');
+    notes.addNote(argv.title, argv.body);
 } else if (command === 'list') {
-    console.log('Listing all notes');
+    notes.getAll();
 } else if (command === 'read') {
-    console.log('Reading note')
+    notes.getNote(argv.title);
 } else if (command === 'remove') {
-    console.log('Remove note');
+    notes.removeNote(argv.title);
 } else if (typeof command === 'undefined') {
     console.log('No Command');
 } else {
