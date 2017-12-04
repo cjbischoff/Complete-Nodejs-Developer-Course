15eeb7953cc0655ce86bb4ddbe0237c579f49d7b
Christopher.Bischoff <christopher.bischoff@phishme.com>
2017-12-04 15:47:11 -0700

    update/notes-node 3-16
    - added functionaility to add/save notes
    - some error checking

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,7 +1,30 @@
 console.log('Starting notes.js');
 
+const fs = require('fs');
+
+
 var addNote = (title, body) => {
-    console.log('Adding note ', title, body);
+    var notes = [];
+    var note = {
+        title,
+        body
+    };
+
+    try {
+        // Load existing notes
+        var notesString = fs.readFileSync('notes-data.json');
+        notes = JSON.parse(notesString);
+    } catch (e) {
+
+    }
+
+    var duplicateNotes = notes.filter((note) => note.title === title);
+
+    if (duplicateNotes.length === 0) {
+        //push new note
+        notes.push(note);
+        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    };
 
 };
 
