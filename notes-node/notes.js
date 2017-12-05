f4bfef1f018410ad95e80cb1195d4ea721d452ec
Christopher.Bischoff <christopher.bischoff@phishme.com>
2017-12-05 13:30:50 -0700

    update/notes-node 3-17
    - refactored
    - added checking for existing note via title

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -3,27 +3,35 @@ console.log('Starting notes.js');
 const fs = require('fs');
 
 
+var fetchNotes = () => {
+    try {
+        var notesString = fs.readFileSync('notes-data.json');
+        return notes = JSON.parse(notesString);
+    } catch (e) {
+        return [];
+    }
+
+};
+
+var saveNotes = (notes) => {
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+
+};
+
 var addNote = (title, body) => {
-    var notes = [];
+    var notes = fetchNotes();
     var note = {
         title,
         body
     };
 
-    try {
-        // Load existing notes
-        var notesString = fs.readFileSync('notes-data.json');
-        notes = JSON.parse(notesString);
-    } catch (e) {
-
-    }
-
     var duplicateNotes = notes.filter((note) => note.title === title);
 
     if (duplicateNotes.length === 0) {
-        //push new note
         notes.push(note);
-        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+        saveNotes(notes);
+        return note;
+
     };
 
 };
